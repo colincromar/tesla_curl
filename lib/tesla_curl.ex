2ed6924c0b1a311d404d521e26a69c87e4e974ba
Colin Cromar <colincromar@gmail.com>
2022-10-23 18:06:49 -0600

    rough implementation of dynamic flag setting based on content-type

--- lib/tesla_curl.ex
+++ lib/tesla_curl.ex
@@ -21,8 +21,9 @@ defmodule Tesla.Middleware.Curl do
   # Calls parsing functions for headers and body, constructs the Curl command and logs it.
   @spec log_request(Tesla.Env.t(), keyword() | nil) :: :ok
   defp log_request(env, opts) do
+    flag_type = get_flag_type(env.headers)
     headers = parse_headers(env.headers, opts)
-    body = parse_body(env.body, opts)
+    body = parse_body(env.body, flag_type, opts)
 
     # In general, concatenation, while not as pretty as interpolation, is a slightly more efficient
     # due to less protocol overhead. As such, this library opts to use concatenation.
@@ -79,40 +80,52 @@ defmodule Tesla.Middleware.Curl do
   end
 
   # Top-level function to parse body
-  @spec parse_body(list(), keyword() | nil) :: String.t()
-  defp parse_body(nil, _opts), do: ""
-  defp parse_body([], _opts), do: ""
+  @spec parse_body(list() | nil, String.t(), keyword() | nil) :: String.t()
+  defp parse_body(nil, _flag_type, _opts), do: ""
+  defp parse_body([], _flag_type, _opts), do: ""
 
-  defp parse_body(body, opts) do
+  defp parse_body(body, flag_type, opts) do
     Enum.map(body, fn {k, v} ->
-      filter_body(k, v, opts)
+      filter_body(flag_type, k, v, opts)
     end)
     |> Enum.join(" ")
   end
 
   # Reads the redact_fields option to find body fields to redact
-  @spec filter_body(String.t(), String.t(), keyword() | nil) :: String.t()
-  defp filter_body(key, value, nil), do: print_field(key, value, false)
+  @spec filter_body(String.t(), String.t(), String.t(), keyword() | nil) :: String.t()
+  defp filter_body(flag_type, key, value, nil), do: print_field(flag_type, key, value, false)
 
-  defp filter_body(key, value, opts) do
+  defp filter_body(flag_type, key, value, opts) do
     with {:ok, redact_fields} <- Keyword.fetch(opts, :redact_fields) do
       fields = Enum.map(redact_fields, fn field -> String.downcase(field) end)
-      print_field(key, value, Enum.member?(fields, String.downcase(key)))
+      print_field(flag_type, key, value, Enum.member?(fields, String.downcase(key)))
     else
-      _ -> print_field(key, value, false)
+      _ -> print_field(flag_type, key, value, false)
     end
   end
 
   # Constructs the body string
-  @spec print_field(String.t(), String.t(), boolean()) :: String.t()
-  defp print_field(key, value, false) do
-    "--data-urlencode '" <> key <> "=" <> value <> "'"
+  @spec print_field(String.t(), String.t(), String.t(), boolean()) :: String.t()
+  defp print_field(flag_type, key, value, false) do
+    flag_type <> " '" <> key <> "=" <> value <> "'"
   end
 
-  defp print_field(key, _value, true) do
-    "--data-urlencode '" <> key <> "=[REDACTED]'"
+  defp print_field(flag_type, key, _value, true) do
+    flag_type <> " '" <> key <> "=[REDACTED]'"
   end
 
+  # Determines the flag type based on the content type header
+  @spec get_flag_type(list()) :: String.t()
+  defp get_flag_type(headers) do
+    content_type = Enum.find(headers, fn {key, _val} -> key == "Content-Type" end)
+    case content_type do
+      {"Content-Type", "application/x-www-form-urlencoded"} -> "--data-urlencode"
+      {"Content-Type", "multipart/form-data"} -> "--form"
+      _ -> "--data"
+    end
+  end
+
+  # Implements a space function to avoid adding a space when the header or body is empty
   @spec space(list()) :: String.t()
   defp space(nil), do: ""
   defp space([]), do: ""
