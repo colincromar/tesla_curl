29bbbb603c07b5a4ba3bc63cc6018ec408d288ed
Colin Cromar <colincromar@gmail.com>
2022-11-09 22:49:09 -0700

    raw response body handling (#9)
    * raw response body handling
    
    * handle head requests
    
    * change normalize to translate
    
    * remove jason dependency

--- lib/tesla_curl.ex
+++ lib/tesla_curl.ex
@@ -50,7 +50,22 @@ defmodule Tesla.Middleware.Curl do
       end)
       |> Enum.join(" ")
 
-    "curl --POST #{headers}#{space(env.headers)}#{parsed_parts} #{env.url}#{format_query_params(query_params)}"
+    "curl -X POST #{headers}#{space(env.headers)}#{parsed_parts} #{env.url}#{format_query_params(query_params)}"
+  end
+
+  defp construct_curl(%Tesla.Env{query: []} = env, opts) when is_binary(env.body) do
+    flag_type = get_flag_type(env.headers)
+    headers = parse_headers(env.headers, opts)
+
+    "curl #{translate_method(env.method)}#{headers}#{space(env.headers)}#{flag_type} '#{env.body}' #{env.url}"
+  end
+
+  defp construct_curl(%Tesla.Env{} = env, opts) when is_binary(env.body) do
+    flag_type = get_flag_type(env.headers)
+    headers = parse_headers(env.headers, opts)
+    query_params = Enum.into(env.query, %{}) |> URI.encode_query(:rfc3986)
+
+    "curl #{translate_method(env.method)}#{headers}#{space(env.headers)}#{flag_type} #{env.body.data} #{env.url}#{format_query_params(query_params)}"
   end
 
   defp construct_curl(%Tesla.Env{} = env, opts) do
@@ -59,7 +74,7 @@ defmodule Tesla.Middleware.Curl do
     body = parse_body(env.body, flag_type, opts)
     query_params = Enum.into(env.query, %{}) |> URI.encode_query(:rfc3986)
 
-    "curl --#{normalize_method(env.method)} #{headers}#{space(env.headers)}#{body}#{space(env.body)}#{env.url}#{format_query_params(query_params)}"
+    "curl #{translate_method(env.method)}#{headers}#{space(env.headers)}#{body}#{space(env.body)}#{env.url}#{format_query_params(query_params)}"
   end
 
   # Top-level function to parse headers
@@ -144,12 +159,18 @@ defmodule Tesla.Middleware.Curl do
     end
   end
 
-  # Converts method atom into a string.
-  @spec normalize_method(atom) :: String.t()
-  defp normalize_method(method) when is_atom(method) do
-    method
-    |> Atom.to_string()
-    |> String.upcase()
+  # Converts method atom into a string and assigns proper flag prefixes
+  @spec translate_method(atom) :: String.t()
+  defp translate_method(:get), do: ""
+  defp translate_method(:head), do: "-I "
+
+  defp translate_method(method) when is_atom(method) do
+    translated =
+      method
+      |> Atom.to_string()
+      |> String.upcase()
+
+    "-X #{translated} "
   end
 
   # Implements a space function to avoid adding a space when the header or body is empty
