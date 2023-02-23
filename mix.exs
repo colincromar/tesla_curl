eac425c006501dfc6b11479b8a1254915ca2603b
Colin Cromar <colincromar@gmail.com>
2023-02-23 11:35:59 -0700

    0.0.1 rc.1 (#34)
    * prepare mix.exs for 0.0.1-rc.1
    
    * prepare README
    
    * typo
    
    * add ex_docs
    
    * set to only dev

--- mix.exs
+++ mix.exs
@@ -36,7 +36,7 @@ defmodule TeslaCurl.MixProject do
   defp deps do
     [
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
-      {:ex_doc, ">= 0.0.0", only: [:docs]},
+      {:ex_doc, ">= 0.0.0", only: [:dev], runtime: false},
       {:tesla, "~> 1.4"}
     ]
   end
